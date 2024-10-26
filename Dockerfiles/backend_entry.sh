2625b74bf1c44e1e6edde0959eaad2c04d6bff24
Totto16 <tobiausgais@gmail.com>
2024-10-27 01:57:55 +0200

    build: fix docker compose execution:
    don't generate prisma client at runtime, but at compile time
    use correct js entry point

--- Dockerfiles/backend_entry.sh
+++ Dockerfiles/backend_entry.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-npx prisma db push
+npx prisma db push --skip-generate
 
 # Note: The fast termination doesn't work in node:x-alpine, I tried everything, but no --init or tini -- solved the problem :(
-exec node src/main.js
+exec node main.js
