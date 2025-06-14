41534fc6a60ef15cd982756c409271165ec436a8
Totto16 <tobiausgais@gmail.com>
2025-06-14 20:39:34 +0200

    fix: add prisma migartions: - add baseline and normal migrations to the backend dockerfile entry - no migrations was necessary for the prisma 5 to 6 as we already used 'relationMode = "foreignKeys"' before this major update (See https://www.prisma.io/docs/orm/more/upgrade-guides/upgrading-versions/upgrading-to-prisma-6 ) -  add openssl to the docker image, as prisma needs it

--- Dockerfiles/backend_entry.sh
+++ Dockerfiles/backend_entry.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+# TODO: check if this works correctly
+# this is the baseline for migrations, which means, this was the schema before we started using migartions
+npx prisma migrate resolve --applied 0_init
+# run migrations, if there are any
+npx prisma migrate deploy
+# push the schema to the db, for new dbs instances, otherwise a noop
 npx prisma db push --skip-generate
 
 # Note: The fast termination doesn't work in node:x-alpine, I tried everything, but no --init or tini -- solved the problem :(
